8af4c7ac00a971c5be10e871fb59311300491adb
Adam <dev.adammilbrath@gmail.com>
2020-01-10 00:54:05 -0600

    created another grid. placed that in TheGrid class. have been working in Ships.

--- BattleShip/Program.cs
+++ BattleShip/Program.cs
@@ -6,33 +6,9 @@ namespace BattleShip
     {
         static void Main(string[] args)
         {
-
-            //static void TheGrid()
-            //{
-            //    int[,] grid = new int[10, 10];
-            //    for (int y = 0; y < 10; y++)
-            //    {
-            //        for (int x = 0; x < 10; x++)
-            //        {
-            //            grid[x, y] = x * y;
-            //        }
-            //    }
-            //}
-
-            string[,] grid = new string[10, 10] { { "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]" }, { "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]" }, { "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]" }, { "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]" }, { "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]" }, { "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]" }, { "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]" }, { "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]" }, { "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]" }, { "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]" } };
-
-            int rowLength = grid.GetLength(0);
-            int colLength = grid.GetLength(1);
-
-            for (int i = 0; i < rowLength; i++)
-            {
-                for (int j = 0; j < colLength; j++)
-                {
-                    Console.Write(string.Format("{0} ", grid[i, j]));
-                }
-                Console.Write(Environment.NewLine + Environment.NewLine);
-            }
+            Console.WriteLine("The 5 ships are: Carrier(occupies 5 spaces), Battleship(4), Cruiser(3), Submarine(3), and Destroyer(2). Please press any button to continue.");
             Console.ReadLine();
+            TheGrid.Run();
         }
     }
 }
---
+++ BattleShip/Ships.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BattleShip
+{
+    //Carrier(occupies 5 spaces), Battleship(4), Cruiser(3), Submarine(3), and Destroyer(2)
+    public class Ships
+    {
+        Ships ship;
+        public string shipName;
+        public int shipSize;
+        public bool isHit;
+        public bool isSunk;
+        
+        public void ShipHit()
+        {
+            isHit = true;
+        }
+        public void ShipSunk()
+        {
+            isSunk = true;
+        }
+
+        //List<Ships> ships = new List<Ships>()
+        //{
+        //    ships.Add(CreateCarrier);
+        //};
+
+        public void CreateCarrier()
+        {
+            Ships carrier = new Ships();
+            carrier.shipName = "Carrier";
+            carrier.shipSize = 5;
+            carrier.isHit = false;
+            carrier.isSunk = false;
+        }
+        public void CreateBattleShip()
+        {
+            Ships battleship = new Ships();
+            battleship.shipName = "BattleShip";
+            battleship.shipSize = 5;
+            battleship.isHit = false;
+            battleship.isSunk = false;
+        }
+        public void CreateCruiser()
+        {
+            Ships cruiser = new Ships();
+            cruiser.shipName = "Cruiser";
+            cruiser.shipSize = 5;
+            cruiser.isHit = false;
+            cruiser.isSunk = false;
+        }
+        public void CreateSubmarine()
+        {
+            Ships submarine = new Ships();
+            submarine.shipName = "Submarine";
+            submarine.shipSize = 5;
+            submarine.isHit = false;
+            submarine.isSunk = false;
+        }
+        public void CreateDestroyer()
+        {
+            Ships destroyer = new Ships();
+            destroyer.shipName = "Destroyer";
+            destroyer.shipSize = 5;
+            destroyer.isHit = false;
+            destroyer.isSunk = false;
+        }
+    }
+}
+//Carrier(occupies 5 spaces), Battleship(4), Cruiser(3), Submarine(3), and Destroyer(2)
